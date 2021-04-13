5c9bdca6991922cbf07c7a49ed1d19a0b1089253
Igor Novoseltsev <inovoselcev@clevertec.ru>
2021-04-13 18:29:42 +0300

    [RepoApp]: ADDED empty view

--- RepoApp/Features/Lists/Controllers/RAListViewController.swift
+++ RepoApp/Features/Lists/Controllers/RAListViewController.swift
@@ -8,18 +8,44 @@
 import UIKit
 
 class RAListViewController: RABaseViewController {
+
+    // MARK: - Variables
+    private let edgeInsets = UIEdgeInsets(top: 16, left: 48, bottom: 16, right: 48)
+
+    // MARK: - GUI Variables
+    private lazy var errorView: RAEmptyListView = {
+        let view = RAEmptyListView()
+        view.setImage(imageName: "empty_folder", alpha: 0.1)
+        view.setDescription("No repositopries here")
+
+        return view
+    }()
+
     // MARK: - Life Cicle
     override func singleDidAppear() {
         super.singleDidAppear()
 
+        // TODO: - Start requests + Dispatch group + Remove next line
         Swift.debugPrint("Start flow")
     }
 
+    // MARK: - Initialization
     override func initController() {
         super.initController()
         self.setContentScrolling(isEnabled: false)
-
         self.controllerTitle = "Repositories"
+
+        self.mainView.addSubview(self.errorView)
+
+        self.constraints()
+    }
+
+    // MARK: - Constraints
+    private func constraints() {
+        self.errorView.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.left.right.equalToSuperview().inset(self.edgeInsets)
+        }
     }
 }
 
--- RepoApp/Features/Lists/Views/RAEmptyListView.swift
+++ RepoApp/Features/Lists/Views/RAEmptyListView.swift
@@ -5,4 +5,46 @@
 //  Created by Igor Novoseltsev on 13.04.21.
 //
 
-import Foundation
+import UIKit
+
+class RAEmptyListView: RAViewWithStackView {
+    // MARK: - Variables
+    private let descriptionInset = UIEdgeInsets(all: 16)
+    var defaultImageSize = CGSize(width: 175, height: 175) {
+        didSet {
+            self.imageView.imageSize = self.defaultImageSize
+        }
+    }
+
+    // MARK: - GUI Variables
+    private lazy var imageView = RAImageView()
+
+    private(set) lazy var descriptionView: RADescriptionView = {
+        let view = RADescriptionView()
+        view.set(text: "No repositories")
+        view.font = .font(ofType: .smallRegular)
+        view.isHidden = true
+
+        return view
+    }()
+
+    // MARK: - Initialization
+    override func initView() {
+        super.initView()
+
+        self.add(self.imageView)
+        self.add(self.descriptionView.injectedIntoContainerWith(edgeInsets: self.descriptionInset))
+    }
+
+    // MARK: - Setters
+    func setImage(imageName: String, alpha: CGFloat = 1) {
+        self.imageView.setImage(name: imageName)
+        self.imageView.imageSize = self.defaultImageSize
+        self.imageView.alpha = alpha
+    }
+
+    func setDescription(_ text: String) {
+        self.descriptionView.isHidden = false
+        self.descriptionView.set(text: text, font: .font(ofType: .smallRegular))
+    }
+}
